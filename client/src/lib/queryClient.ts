5df0e8eee77c15b93f81327158787a8a4e4732ab
abobakeralsaraf <bakry@abobakeralsaraf.com>
2025-12-13 20:53:11 +0200

    Fix async/await syntax in getQueryFn arrow function

--- client/src/lib/queryClient.ts
+++ client/src/lib/queryClient.ts
@@ -28,7 +28,7 @@ type UnauthorizedBehavior = "returnNull" | "throw";
 export const getQueryFn: <T>(options: {
   on401: UnauthorizedBehavior;
 }) => QueryFunction<T> =
-  ({ on401 }: UnauthorizedBehavior) =>
+({ on401 }: UnauthorizedBehavior) =>
   async (({ queryKey }: { queryKey: unknown[] }) => {
     const res = await fetch(`${API_URL}/${(queryKey as string[]).join("/")}`, {
       credentials: "include",
